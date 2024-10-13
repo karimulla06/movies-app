ede0d4b06aceb76411878dcb4e3866758414a961
Karimulla Mohammed <mdkarim1266@gmail.com>
2024-10-14 01:48:42 +0530

    updated favorites

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -3,10 +3,19 @@ import { getFavorites } from "@/utils";
 import styles from "./favorites.module.css";
 
 const Favorites = () => {
+  const favorites = getFavorites();
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Your Favorites</h2>
-      <MoviesList movies={getFavorites()} />
+      {favorites.length > 0 ? (
+        <>
+          <h2 className={styles.title}>Your Favorites</h2>
+          <MoviesList movies={favorites} />
+        </>
+      ) : (
+        <p className={styles.no_favorites}>
+          You haven't added any favorites yet
+        </p>
+      )}
     </div>
   );
 };
