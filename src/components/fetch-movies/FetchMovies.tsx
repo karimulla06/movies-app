cad46853badfaeadd46edd6128fd8c4ef30f30e7
Karimulla Mohammed <mdkarim1266@gmail.com>
2024-10-15 21:50:05 +0530

    fixed error message

--- src/components/fetch-movies/FetchMovies.tsx
+++ src/components/fetch-movies/FetchMovies.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { MoviesList } from "@/components/movies-list/MoviesList";
 import { Movie, MoviesApiResponse } from "@/types";
 
+import styles from "./fetch-movies.module.css";
+import { translation_keys } from "@/constants";
+
 interface FetchMoviesProps {
   fetchFn: (page: number) => Promise<MoviesApiResponse>;
 }
@@ -10,6 +13,7 @@ export const FetchMovies = ({ fetchFn }: FetchMoviesProps) => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const fetchMovies = async () => {
     try {
@@ -18,7 +22,7 @@ export const FetchMovies = ({ fetchFn }: FetchMoviesProps) => {
       setMovies([...movies, ...results]);
       setPage(currentPage == total_pages ? 0 : page + 1);
     } catch (error) {
-      console.error(error);
+      setError(true);
     } finally {
       setIsLoading(false);
     }
@@ -27,6 +31,16 @@ export const FetchMovies = ({ fetchFn }: FetchMoviesProps) => {
   useEffect(() => {
     fetchMovies();
   }, []);
+
+  if (isLoading && movies.length === 0) {
+    return <p className={styles.message}>{translation_keys.loading}</p>;
+  }
+
+  if (error) {
+    return (
+      <p className={styles.message}>{translation_keys.error_fetching_movies}</p>
+    );
+  }
 
   return (
     <MoviesList
